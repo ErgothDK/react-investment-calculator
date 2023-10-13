a826f97228d01d7cd88e457fcc11ff5932475fc7
ErgothDK <ergothdk@gmail.com>
2023-10-12 18:00:04 -0600

    Event handling added for reset button, submit and input change

--- src/Components/InvestmentForm/InvestmentForm.js
+++ src/Components/InvestmentForm/InvestmentForm.js
@@ -2,16 +2,37 @@ import React from "react";
 import styles from "./InvestmentForm.module.css";
 
 const InvestmentForm = () => {
+  function resetClickHandler() {
+    console.log("reset clicked");
+  }
+
+  function submitHandler(event) {
+    event.preventDefault();
+    console.log("submit clicked");
+  }
+
+  function inputChangeHandler(event) {
+    console.log(event.target.id, event.target.value);
+  }
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={submitHandler}>
       <div className={styles["input-group"]}>
         <p>
           <label htmlFor="current-savings">Current Savings ($)</label>
-          <input type="number" id="current-savings" />
+          <input
+            type="number"
+            id="current-savings"
+            onChange={inputChangeHandler}
+          />
         </p>
         <p>
           <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
-          <input type="number" id="yearly-contribution" />
+          <input
+            type="number"
+            id="yearly-contribution"
+            onChange={inputChangeHandler}
+          />
         </p>
       </div>
       <div className={styles["input-group"]}>
@@ -19,15 +40,23 @@ const InvestmentForm = () => {
           <label htmlFor="expected-return">
             Expected Interest (%, per year)
           </label>
-          <input type="number" id="expected-return" />
+          <input
+            type="number"
+            id="expected-return"
+            onChange={inputChangeHandler}
+          />
         </p>
         <p>
           <label htmlFor="duration">Investment Duration (years)</label>
-          <input type="number" id="duration" />
+          <input type="number" id="duration" onChange={inputChangeHandler} />
         </p>
       </div>
       <p className={styles.actions}>
-        <button type="reset" className={styles.buttonAlt}>
+        <button
+          type="reset"
+          className={styles.buttonAlt}
+          onClick={resetClickHandler}
+        >
           Reset
         </button>
         <button type="submit" className={styles.button}>
